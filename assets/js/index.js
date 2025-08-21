46373a267e1794187992678fb3b4fd03cd3edd59
Swaraj Roy <swarajroy2006@gmail.com>
2025-08-21 22:14:37 +0530

    Update Structure

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+
+
+var typed = new Typed(".greet",{
+    strings: ["Happy Birthday", "শুভ জন্মদিন", "जन्मदिन मुबारक हो", "Joyeux Anniversaire", "お誕生日おめでとう", "Alles Gute zum Geburtstag"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
